b9a82da80324484e41012d6b9c723bc0581b207a
K0stka <www.honza.90@gmail.com>
2023-12-25 17:10:57 +0100

    Force FTP reupload?

--- php/conf.php
+++ php/conf.php
@@ -23,7 +23,7 @@ if (DEV) {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
 
-    define("SERVICE_WORKER_ENABLED", true);
+    define("SERVICE_WORKER_ENABLED", false);
 
     $v = "?v=1";
 
