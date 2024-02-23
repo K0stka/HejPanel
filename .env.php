4c6c060d17ffee6bb1e00e27e19cfd11e661ce26
K0stka <www.honza.90@gmail.com>
2024-02-23 20:23:33 +0100

    Fixed the .env.php file

---
+++ .env.php
@@ -0,0 +1,11 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "hejpanel");
+define("DB_PASSWORD", "hejpanel");
+define("DB_NAME", "hejpanel");
+
+define("ADMIN_NAME", "admin username");
+define("ADMIN_PASSWORD", "admin password");
+
+define("PRIVATE_KEY", "can be left empty");
+define("PUBLIC_KEY", "can be left empty");
